4ad7f828df761c3c4fe2bb6d4e9d418514401f09
moyamo <moyamodehacker@gmail.com>
2015-04-25 23:40:03 +0200

    Keep track of winners and losers

--- chain-reaction.js
+++ chain-reaction.js
@@ -3,11 +3,11 @@ var CReact = {
 	// The canvas that the game runs in.
 	canvas: undefined,
 	// Number of columns of the board.
-    columns: 8,
+	columns: 6,
 	// Number of rows of the board.
-	rows: 8,
+	rows: 5,
 	// Number of players in the game.
-	players: 2,
+	players: 3,
 	// Represents the gameboard.
 	gameBoard: undefined,
 	// Number of the player whose turn it is.
@@ -15,9 +15,13 @@ var CReact = {
 	// Time to wait between animations in milliseconds.
 	animationDelay: 700,
 	// playerColors[i] is the color of the ith player.
-	playerColors : ['#000000', '#FF0000', '#00FF00'],
+	playerColors : ['#000000', '#FF0000', '#00FF00', '#0000FF'],
 	// True if an atom can be placed on the board. Used for locking.
 	canPlay: true,
+	// True if player has made their first move
+	hasPlayed: [],
+	// True if player has lost the game. (He made a move and lost all atoms)
+	hasLost: [],
 };
 
 /**
@@ -52,6 +56,12 @@ function loadGame(canvasId) {
 		}
 	}
 	drawBoard(CReact.gameBoard, CReact.canvas, CReact.rows, CReact.columns);
+	CReact.hasPlayed = [];
+	CReact.hasLost = [];
+	for (var i = 0; i <= CReact.players; ++i) {
+		CReact.hasPlayed.push(false);
+		CReact.hasLost.push(false);
+	}
 	CReact.canPlay = true;
 
 	// Find out which cell was clicked and notify board
@@ -74,6 +84,40 @@ function loadGame(canvasId) {
 	});
 }
 
+/** Updates the hasLost list based on gameBoard */
+function updateLoser()
+{
+	var board = CReact.gameBoard;
+	// If they have played they might have lost
+	for (var i = 0; i <= CReact.players; ++i) {
+		CReact.hasLost[i] = CReact.hasPlayed[i];
+	}
+	// If they own a square they have not lost
+	for (var i = 0; i < CReact.rows; ++i) {
+		for (var j = 0; j < CReact.columns; ++j) {
+			CReact.hasLost[board[i][j].player] = false;
+		}
+	}
+}
+
+/** Returns the player that has won or zero if there is no winner yet */
+function findWinner()
+{
+	var count = 0;
+	var winner = 0;
+	for (var i = 1; i <= CReact.players; ++i) {
+		if (!CReact.hasLost[i]) {
+			winner = i;
+			count++;
+		}
+	}
+	if (count == 1) {
+		return winner;
+	} else {
+		return 0;
+	}
+}
+
 /**
  * Called when cell is clicked.
  *
@@ -88,20 +132,37 @@ function onCellClick(cellcoord) {
 	if (cell.player == 0 || cell.player == turn) {
 		cell.numAtoms += 1;
 		cell.player = turn;
-		CReact.turn = turn % CReact.players + 1;
+		if (!CReact.hasPlayed[turn]) {
+			CReact.hasPlayed[turn] = true;
+		}
+		// Cycle players until we find one who hasn't lost
+		do {
+			CReact.turn = CReact.turn % CReact.players + 1;
+		} while (CReact.hasLost[CReact.turn])
+
 		var draw = function () {
 			drawBoard(CReact.gameBoard, CReact.canvas,
 					CReact.rows, CReact.columns);
 		};
+		// Explode and pause for the animation
 		var explodeAndWait = function (queue) {
 			queue = handleExplosions(queue, CReact.gameBoard);
 			window.requestAnimationFrame(draw);
-			if (queue.length > 0) {
+			// Check if there is a winner
+			updateLoser(); // See who has lost
+			var winner = findWinner();
+			if (winner != 0) {
 				setTimeout(function() {
-					explodeAndWait(queue);
+					drawWinner(winner);
 				}, CReact.animationDelay);
 			} else {
-				CReact.canPlay = true;
+				if (queue.length > 0) {
+					setTimeout(function() {
+						explodeAndWait(queue);
+					}, CReact.animationDelay);
+				} else {
+					CReact.canPlay = true;
+				}
 			}
 		};
 		explodeAndWait([cellcoord]);
@@ -174,6 +235,25 @@ function computeCellWidth(canvas, rows, columns) {
 	var maxWidth = canvas.width / columns;
 	var maxHeight = canvas.height / rows;
 	return Math.floor(Math.min(maxWidth, maxHeight));
+}
+
+/**
+ * Clears the board and draws congratulations for the winner.
+ *
+ * Arguments
+ *   winner - the winner
+ */
+function drawWinner(winner) {
+	var canvas = CReact.canvas;
+	var ctx = canvas.getContext("2d");
+	var width = canvas.width;
+	var height = canvas.height;
+	var fontSize = Math.floor(width / 7);
+	var text = "Player " + winner.toString() + " wins!";
+	ctx.font = fontSize.toString() + "px sans";
+	ctx.clearRect(0, 0, width, height);
+	ctx.fillStyle = CReact.playerColors[winner];
+	ctx.fillText(text, 0, height / 2);
 }
 
 /**
